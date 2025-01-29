3b8cdc9f6598bd005b614f7c6485b2ad779ecb8b
KoichiroGyoji <toko.gyoji.1210@gmail.com>
2025-01-29 11:31:17 +0900

    fifth commit

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KcgAgriController;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+
+Route::get('/', function () {
+    return view('agris.home');
+});
 
 // Route::get('/', [KcgAgriController::class, 'home']);
